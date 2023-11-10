136ba49ce3e81128fc1a1ea86478fceedaf54b1a
Andres Altuve <hi@andresaltuve.com>
2023-11-10 01:08:10 -0400

    Breaking Bad first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Breaking Bad Challenge 
+
+This challenge was really challenging and a lot of fun too! 
+
+
+![Breaking Bad challege](./breakingBad.gif)
+
+# Built With ViteJS
+
+## Installation
+
+1. Clone the repository<br>
+git clone https://github.com/AndresAltv/Dots-challenge.git<br><br>
+
+2. Install dependencies<br>
+npm install<br><br>
+
+3. Start server<br>
+npm run dev
---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from 'react';
+import elements from './data/elements';
+
+type Letter = {
+  letter: string,
+  green: boolean
+}
+
+export const App: React.FC = () => {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [nameArr, setNameArr] = useState<Letter[]>([]);
+  const [lastArr, setLastArr] = useState<Letter[]>([]);
+
+  const handleClear = (): void => {
+    setFirstName("");
+    setLastName("");
+    setNameArr([]);
+    setLastArr([]);
+  }
+
+  const checkLetter = (word: string): Letter[] => {
+
+    let placeholder: Letter[] = [];
+    for (let i: number = 0; i < word.length; i++) {
+      placeholder.push({ letter: word[i], green: false })
+    }
+
+    if (placeholder.length === 1) {
+      let letter = placeholder[0].letter;
+      let find: number = elements.findIndex((el) => el === letter);
+      if (find !== -1) { placeholder[0].green = true }
+    }
+    if (placeholder.length > 1) {
+      let letters = placeholder[0].letter + placeholder[1].letter;
+      let find: number = elements.findIndex((el) => el === letters);
+      if (find !== -1) {
+        placeholder[0].green = true;
+        placeholder[1].green = true;
+      }
+    }
+    return placeholder;
+
+  }
+
+  useEffect(() => {
+
+    let check = checkLetter(firstName);
+    setNameArr(check);
+
+  }, [firstName])
+
+  useEffect(() => {
+    let check = checkLetter(lastName);
+    setLastArr(check);
+  },[lastName])
+
+  return (
+
+    <div className="container">
+
+      <div className="strings">
+        {
+          nameArr.length !== 0 &&
+          nameArr.map((nam) =>
+            !nam.green ? nam.letter : <span>{nam.letter}</span>
+          )
+        }
+      </div>
+      <div id="stringLast" className="strings">
+        {
+          lastArr.length !== 0 &&
+          lastArr.map((nam) =>
+            !nam.green ? nam.letter : <span>{nam.letter}</span>
+          )
+        }
+      </div>
+
+      <input id="firstInput" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+      <input id="lastInput" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+      <div className="actions">
+        <button onClick={handleClear}>Clear</button>
+      </div>
+    </div>
+
+  )
+
+}
+
+
+export default App
